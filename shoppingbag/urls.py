da488cc73c2b6f633a899b255a58fbac172fa0e8
Dorin Stanciu <stanciudorin@yahoo.com>
2020-11-23 21:46:26 +0000

    Added ad to bag functionality

--- shoppingbag/urls.py
+++ shoppingbag/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.view_shoppingbag, name="view_shoppingbag")
+    path("", views.view_shoppingbag, name="view_shoppingbag"),
+    path("add/<item_id>/", views.add_to_bag, name="add_to_bag"),
 ]
