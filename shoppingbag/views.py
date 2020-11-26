03d220953daf6d41322cdffc117b2e292f26ee30
Dorin Stanciu <stanciudorin@yahoo.com>
2020-11-26 21:13:48 +0000

    Added adjust bag functionality and calculating the subtotal

--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_shoppingbag(request):
@@ -20,3 +20,35 @@ def add_to_bag(request, item_id):
 
     request.session['shoppingbag'] = shoppingbag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust the quantity of the specified product to the specified amount """
+
+    quantity = int(request.POST.get('quantity'))
+    # redirect_url = request.POST.get('redirect_url')
+    shoppingbag = request.session.get('shoppingbag', {})
+
+    if quantity > 0:
+        shoppingbag[item_id] = quantity
+    else:
+        shoppingbag.pop(item_id)
+
+    request.session['shoppingbag'] = shoppingbag
+    return redirect(reverse('view_bag'))
+    
+
+def remove_from_bag(request, item_id):
+    """ Remove the item from the shoppind bag """
+
+    
+    redirect_url = request.POST.get('redirect_url')
+    shoppingbag = request.session.get('shoppingbag', {})
+    try:
+        if quantity == 0:
+            shoppingbag.pop(item_id)
+            
+        request.session['shoppingbag'] = shoppingbag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
