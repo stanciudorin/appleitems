171701eba02360d9daaf07fb342d721dbebd6fbc
Dorin Stanciu <stanciudorin@yahoo.com>
2020-12-06 11:14:54 +0000

    Finalizing the image field

--- products/views.py
+++ products/views.py
@@ -101,8 +101,8 @@ def add_product(request):
 def edit_product(request, product_id):
     """ Edit a product in the store """
     if not request.user.is_superuser:
-    messages.error(request, 'Sorry, only store owners can do that.')
-    return redirect(reverse('home'))
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
